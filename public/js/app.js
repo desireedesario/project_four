cd6f634f03e8d06d7f73ac70cc5b2d2182e504d9
TJ Palazzari <tjpalazzari@yahoo.com>
2016-02-16 11:31:21 -0800

    adds navbar customer directive

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
 angular.module('dojoBooks', ['ui.router'])
+  .directive('navBar', navBar)
+
+function navBar(){
+  var directive = {
+    restrict: 'E',
+    templateUrl: '/partials/nav.html',
+    transclude: true
+  }
+  return directive
+}
