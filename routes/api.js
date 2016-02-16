cd1a79ee1635085fd5233b2ee1272ea8927fd81b
TJ Palazzari <tjpalazzari@yahoo.com>
2016-02-16 11:31:39 -0800

    adds user routes

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,20 @@ var
   express          = require('express'),
   apiRouter        = express.Router(),
   mongoose         = require('mongoose'),
-  eventsController = require('../controllers/eventsController.js');
+  eventsController = require('../controllers/eventsController.js'),
+  usersController  = require('../controllers/usersController.js')
   
+
+apiRouter.route('/users')
+  .post(usersController.create)
+
+apiRouter.route('/users')
+  .get(usersController.index)
+
+apiRouter.route('/users/:user_id')
+  .get(usersController.show)
+  .put(usersController.update)
+  .delete(usersController.destroy)
 
 apiRouter.get('/', function(req,res){
   res.json({message: "Api routes are working."})
