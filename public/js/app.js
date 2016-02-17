a479b8b72359093b277b44a8fcd566d52e77f6c1
TJ Palazzari <tjpalazzari@yahoo.com>
2016-02-16 19:24:52 -0800

    makes form directive

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,20 @@
 angular.module('dojoBooks', ['ui.router'])
   .directive('navBar', navBar)
+  .directive('eventForm', eventForm)
 
 function navBar(){
   var directive = {
     restrict: 'E',
     templateUrl: '/partials/nav.html',
     transclude: true
+  }
+  return directive
+}
+
+function eventForm(){
+  var directive = {
+    restrict: 'E',
+    templateUrl: '/partials/event-form.html'
   }
   return directive
 }
